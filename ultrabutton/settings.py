1d4388e8dd3697ea590af73a60daec682ea1fcaa
Nelson Mehlis <nelson.mehlis@gmail.com>
2025-03-24 11:48:23 +0100

    Add STATIC_ROOT setting for static file collection

--- ultrabutton/settings.py
+++ ultrabutton/settings.py
@@ -155,3 +155,6 @@ LOGIN_REDIRECT_URL = '/xp_system/profile/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_LOGIN_METHODS = {'username'}
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
+
+# Location where static files will be collected
+STATIC_ROOT = BASE_DIR / 'staticfiles'
