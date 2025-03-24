482f0c364ab15ba097351a2c1275a295e0ba6792
Nelson Mehlis <nelson.mehlis@gmail.com>
2025-03-24 11:46:15 +0100

    Initial Heroku deployment

--- xp_system/urls.py
+++ xp_system/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('xp-form/', views.xp_form_view, name='xp_form'),
     path('profile/', views.profile_view, name='profile'),
     path('leaderboard/', views.leaderboard_view, name='leaderboard'),
-    
 ]
