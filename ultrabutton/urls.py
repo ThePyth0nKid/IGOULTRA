5ffd7610784146b7c3b033176e0856082190d3ec
Nelson Mehlis <nelson.mehlis@gmail.com>
2025-03-28 16:04:07 +0100

    Add JWT authentication endpoints for token management

--- ultrabutton/urls.py
+++ ultrabutton/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('api/', include('xp_system.urls')),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Allauth URL patterns for login, logout, signup
     path('accounts/', include('allauth.urls')),
