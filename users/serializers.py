4fe5c05e068da6172685255c3d4c9b456560ed2a
Nelson <nelson.mehlis@icloud.com>
2025-03-31 15:12:16 +0200

    Remove __pycache__ directories and compiled Python files from the repository

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,36 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            xp=0,
+            level=1,
+            rank='Bronze',
+        )
+        return user
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'avatar', 'xp', 'level', 'rank']
+        read_only_fields = ['id', 'xp', 'level', 'rank']
